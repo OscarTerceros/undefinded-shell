b11ddb73b7d6f5d1a11fa1793019b432e82f1f46
Oscar Terceros <osterce@gmail.com>
2023-06-12 17:28:10 -0400

    express enviar parametros desde la URL

--- introExpress/index.js
+++ introExpress/index.js
@@ -3,8 +3,15 @@ import express from 'express';
 const app = express();
 
 app.get( "/", ( req, res) => {
-    /* res.send( 'Hello express' ); */
-    res.json( {"message": "Hello express"} );
+    res.json( { message: "Hello World!" } );
+});
+
+app.get( "/hello/:name", ( req, res) => {
+    res.json( { message: `Hello ${ req.params.name }!` } );
+});
+
+app.get( "/add/:num1/:num2", ( req, res) => {
+    res.send( { message: `the sum of ${ req.params.num1 } and ${ req.params.num2 } is ${ parseInt(req.params.num1) + parseInt(req.params.num2) }` });
 });
 
 app.listen( 3001, () => console.log( 'Server running on port 3001' ));
