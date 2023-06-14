b94d113196c83b48df7c5fa478d48f42876097e4
Oscar Terceros <osterce@gmail.com>
2023-06-14 11:54:38 -0400

    create-post

--- introMongo/index.js
+++ introMongo/index.js
@@ -3,35 +3,46 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 const app = express();
 
 app.get( '/', ( req, res ) => {
     res.send( 'API is running!' );
 });
 
-const post = [{
-    id: 1,
-    title: 'Post 1',
-    body: 'This is post 1',
-    userId: 1,
-    tags: ['tag1', 'tag2'],
-    reactions: 2
-}, {
-    id: 2,
-    title: 'Post 2',
-    body: 'This is post 2',
-    userId: 2,
-    tags: ['tag1', 'tag2'],
-    reactions: 2
-}];
+let posts = [
+    {
+        id: 1,
+        title: 'Post 1',
+        body: 'This is post 1',
+        userId: 1,
+        tags: ['tag1', 'tag2'],
+        reactions: 2
+    },
+    {
+        id: 2,
+        title: 'Post 2',
+        body: 'This is post 2',
+        userId: 2,
+        tags: ['tag1', 'tag2'],
+        reactions: 2
+    }];
 
 app.get( '/posts', ( req, res ) => {
-    res.send( post );
+    res.send({ posts });
 });
 
-app.post( '/posts', ( req, res ) => {
-    res.send( 'Products' );
+app.post( '/create-post', ( req, res ) => {
+    const post = {
+        id: posts.length + 1,
+        title: "lorem ipsum",
+        body: "lorem ipsum",
+        userId: 1,
+        tags: ['tag1', 'tag2'],
+        reactions: 2
+    };
+    posts.push( post );
+    res.json({ post });
 });
 
 app.listen( PORT, console.log( `🌎 Server running on port http://localhost:${PORT}` ) );
